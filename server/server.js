8667c5824e7f7f0563035c13756310fce93de8b4
Andres Olivero <andres.eolivero@gmail.com>
2017-03-30 18:08:53 -0700

    feat: added score calculation
    added a post request on the 'submit' page, sending the user's answers stored in session storage. Calculated score on the server and sent it back to the client

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser')
 const testData = require('../src/database.js');
 
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
 app.use(express.static(path.join(__dirname, '../src')));
 app.use(express.static(path.join(__dirname, '../')));
 app.use(function(req, res, next) {
@@ -20,6 +25,21 @@ app.get( '/test', function (req, res) {
 
 app.get( '/submit', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/submit.html'));
+})
+
+app.post('/score', function (req, res) {
+  const answers = req.body;
+  const questions = testData["test1_questions"];
+  const questionsLength = questions.length;
+  let totalCorrect = 0;
+  
+  for (let i = 0; i < questionsLength; ++i) {
+    correctAnswer = questions[i]["correctAnswer"];
+    if (correctAnswer === answers[i]) totalCorrect++;
+  }
+
+  let result = (totalCorrect / questionsLength) * 100;
+  res.send({ "score": result});
 })
 
 app.get('/questions', function (req, res) {
